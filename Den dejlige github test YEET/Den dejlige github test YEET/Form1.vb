0bdd347ac047c52946487da7705975a139a7762f
xXRedbridgeXx <marcusrodbro@gmail.com>
2020-03-23 13:44:52 +0100

    Tilføjet end funktion til knap 3

--- Den dejlige github test YEET/Den dejlige github test YEET/Form1.vb
+++ Den dejlige github test YEET/Den dejlige github test YEET/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         End
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
 End Class
